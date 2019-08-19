916f1a0caa11d3327d05c3ec4acfc90471170420
vanshrishi <vansh.rishi_cs18@gla.ac.in>
2019-08-19 02:15:31 -0700

    add question2.

--- src/question2/Ques2.java
+++ src/question2/Ques2.java
@@ -1,7 +1,13 @@
 package question2;
 
+import java.util.Scanner;
+
 public class Ques2 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String value1=scanner.nextLine().trim();
+        String value2 = new Scanner(System.in);
+        System.out.println(value1+"University"+value2);
+        scanner.close();
     }
 }
